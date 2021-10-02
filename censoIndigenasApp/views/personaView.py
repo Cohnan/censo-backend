1211aaa2894975166f436906d6000c0e89d6b29d
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-02 07:14:25 -0500

    CI-83 Agregado servicio web Inscripcion persona

--- censoIndigenasApp/views/personaView.py
+++ censoIndigenasApp/views/personaView.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 #from django.http import HttpResponse
 from rest_framework.views import APIView # Averiguar
 from rest_framework.response import Response
+from rest_framework import status
 
 from ..models import Persona
 from ..serializers import PersonaSerializer
@@ -13,3 +14,10 @@ class PersonaList(APIView):
         lista_personas = Persona.objects.all()
         serializer = PersonaSerializer(lista_personas, many = True)
         return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = PersonaSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
