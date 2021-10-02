3279bb5efdc9f49a47ad663e390630367397e60e
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-01 23:29:55 -0500

    Agregado view para todas Personas

---
+++ censoIndigenasApp/views/__init__.py
@@ -0,0 +1,1 @@
+from .personaView import PersonaList
---
+++ censoIndigenasApp/views/personaView.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from django.shortcuts import get_object_or_404
+#from django.http import HttpResponse
+from rest_framework.views import APIView # Averiguar
+from rest_framework.response import Response
+
+from ..models import Persona
+from ..serializers import PersonaSerializer
+
+class PersonaList(APIView):
+    def get(self, request):
+        lista_personas = Persona.objects.all()
+        serializer = PersonaSerializer(lista_personas)
+        return Response(sereializer.data)
