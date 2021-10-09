665beedb9e298285b6f125bf92f32759d5ce81cf
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-09 09:56:45 -0500

    Decoraciones modelo Persona

--- censoIndigenasApp/models/persona.py
+++ censoIndigenasApp/models/persona.py
@@ -7,11 +7,11 @@ from censoIndigenasApp.models.resguardo import Resguardo
 
 class Persona(models.Model):
     id = models.BigAutoField(primary_key=True)
-    tipo_doc = models.CharField(max_length = 4)
-    doc_id = models.IntegerField(null = True)
-    id_ocupacion = models.ForeignKey(Ocupacion, related_name='ocupacion',on_delete= models.CASCADE)
+    tipo_doc = models.CharField("Tipo de documento", max_length = 4)
+    doc_id = models.IntegerField("Número de documento", null = True)
+    id_ocupacion = models.ForeignKey(Ocupacion, related_name='Ocupación',on_delete= models.CASCADE)
     nombre = models.CharField('Nombre', max_length = 45)
-    fechadenaciemiento = models.DateField()
-    id_resguardo = models.ForeignKey(Resguardo, related_name='resguardo',on_delete= models.CASCADE)
-    id_etnia = models.ForeignKey(Etnia, related_name='etnia',on_delete= models.CASCADE)
-    departamento = models.CharField('Departamento_Residencia', max_length = 45)
+    fechadenaciemiento = models.DateField("Fecha de Nacimiento")
+    id_resguardo = models.ForeignKey(Resguardo, related_name='Resguardo',on_delete= models.CASCADE)
+    id_etnia = models.ForeignKey(Etnia, related_name='Etnia',on_delete= models.CASCADE)
+    departamento = models.CharField('Departamento de Residencia', max_length = 45)
