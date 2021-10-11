e248a25c1203dceb1202b6c65ba5f968dafe82fd
fabio andres balanta <fabioannypro47@gmail.com>
2021-10-10 23:43:45 -0500

    actualizacion serializador

--- censoIndigenasApp/serializers/usuarioSerializer.py
+++ censoIndigenasApp/serializers/usuarioSerializer.py
@@ -4,15 +4,15 @@ from rest_framework                               import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model  = usuario.Usuario
-        fields = ['id', 'username', 'password', 'name', 'email',]
+        fields = ['id', 'password', 'name', 'email',]
     def create(self, validated_data):
-        userInstance = usuario.Usuario.objects.create(**validated_data)
+        userInstance = usuario.Usuario.objects.create_user(**validated_data)
         return userInstance
 
     def to_representation(self, obj):
         user   = usuario.Usuario.objects.get(id=obj.id)
         return {
-            "username" : user.username,
+           
             "name" : user.name,
             "email" : user.email,
         }
