d07acb15144749e0daaddfb925c682a98dcc2e6d
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-12 00:17:24 -0500

    Arreglado Usuario modelo para no encriptar 2 veces, y para no tener columna is_superuser innecesaria

--- censoIndigenasApp/models/usuario.py
+++ censoIndigenasApp/models/usuario.py
@@ -7,9 +7,14 @@ from django.contrib.auth.hashers import make_password
 from django.core.validators import MinLengthValidator
 
 class UsuarioManager(BaseUserManager):
-    def create_user(self, email, password, nombre):#, es_coordinador = False): 
+    def create_user(self, email, password, nombre, es_admin = False):
         # if not email or not password:
         #     raise ValueError('Los Usuarios deben tener Email y Contraseña')
+
+        # Si en la peticion de registro se indico que el usuario fuera admin, hacer uso del 
+        # metodo creado para eso
+        if es_admin:
+            return self.create_superuser(email = email, password = password, nombre = nombre)
 
         # Usando super clase, poniendo ojala todos los atributos heredados necesarios
         usuario = self.model(
@@ -17,7 +22,9 @@ class UsuarioManager(BaseUserManager):
         )
 
         # Agregando los atributos adicionales al usuario
-        usuario.set_password(password)
+        sal = 'condimento'
+        usuario.password = make_password(password, sal)
+        #usuario.set_password(password)
         usuario.nombre = nombre
 
         # Guardar en base de datos
@@ -41,7 +48,7 @@ class UsuarioManager(BaseUserManager):
 
         return usuario
 
-class Usuario(AbstractBaseUser, PermissionsMixin):
+class Usuario(AbstractBaseUser):
     id = models.BigAutoField(primary_key = True)
     email = models.EmailField(verbose_name = "Email", max_length = 100, unique = True, blank = False, validators=[MinLengthValidator(4)])
     password = models.CharField(verbose_name = "Contraseña", max_length = 256)
@@ -57,7 +64,8 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
     # Estos son los atributos los pedirá django al momento de decirle createsuperuser
     REQUIRED_FIELDS = ['nombre']
 
-    def save(self, **kwargs):
-        sal = 'condimento'
-        self.password = make_password(self.password, sal)
-        super().save(**kwargs)
+    # Solo se debe hacer una encriptacion de la contrasena, y la estamos haciendo en el UserManager
+    # def save(self, **kwargs):
+    #     sal = 'condimento'
+    #     self.password = make_password(self.password, sal)
+    #     super().save(**kwargs)
