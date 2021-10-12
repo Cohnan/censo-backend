4951fef3c357fb0a5e556861566f3416cf3e6234
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-12 01:12:25 -0500

    Mejorado View Gestion Usuarios, empezado Gestion Usuario particular

--- censoIndigenasApp/views/usuarioViews.py
+++ censoIndigenasApp/views/usuarioViews.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.http import Http404
 
 from rest_framework import status
 from rest_framework.views import APIView
@@ -17,8 +18,9 @@ class UsuarioLista(APIView):
     '''
     Vista para la administracion de Usuarios, de parte de un usuario administrador
     '''
-    # Para verificar antes de cada método que estos permiso (IsAuthenticated) se tiene
-    # (y así no tener que hacer manualmente la verificación de que haya un Bearer token)
+    # Para verificar antes de cada método que se tiene el permiso IsAuthenticated
+    # (y así no tener que hacer manualmente la verificación de que haya un Bearer token (código comentado en get),
+    # y de que éste esté vigente )
     # Y que la columna is_staff sea true (IsAdminUser)
     permission_classes = (IsAuthenticated, IsAdminUser)
 
@@ -27,28 +29,17 @@ class UsuarioLista(APIView):
         Traer lista de usuarios
         '''
 
-        # Extraer el token el "Bearer <token>"
-        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        # # Extraer el token el "Bearer <token>"
+        # try :
+        #     token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        # except:
+        #     return Response({"detail": "Se necesita proveer un Bearer Token para autenticacion."}, status = status.HTTP_401_UNAUTHORIZED)
         
-        print(f"\n\n UsuarioLista|get|request.META.get('HTTP_AUTHORIZATION'):\n{request.META.get('HTTP_AUTHORIZATION')} \n\n")
-
-        token_backend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM']) # Setup parser
-        
-        info_token    = token_backend.decode(token, verify=False) # Que hace el verify?
-
-        print(f"\n\n UsuarioLista|get|info_token (token_backend.decode(token):\n{info_token} \n\n")
-
-
-        # Extraer informacion de usuario loggeado
-        usuario_token_id = info_token["user_id"]
-        usuario = Usuario.objects.get(id = usuario_token_id)
-        usuario_serializer = UserSerializer(usuario)
-
-        print(f"\n\n UsuarioLista|get|usuario_serializer.data:\n{usuario_serializer.data} \n\n")
+        # print(f"\n\n UsuarioLista|get|request.META.get('HTTP_AUTHORIZATION'):\n{request.META.get('HTTP_AUTHORIZATION')} \n\n")
 
         # Devolver lista de usuarios
         lista_usuarios = Usuario.objects.all()
-        serializer = UserSerializer(lista_usuarios, many = True)
+        serializer = UserSerializer(lista_usuarios, many = True) # Lista de Jsons con la informacion de cada uno
 
         return Response(serializer.data)
 
@@ -69,4 +60,40 @@ class UsuarioLista(APIView):
         return Response({"detail": "No se pudo crear el usuario", "errors": serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
 
 class UsuarioDetalle(APIView):
-    pass
+    '''
+    Gestionar a un usuario
+    '''
+
+    # Antes de cada metodo: verificar que se esta proveyendo token en la HTTP Request
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, id):
+        '''
+        Extraer modelo de usuario identificado por id dado
+        '''
+        try:
+            return Usuario.objects.get(id=id) # Query SELECT * WHERE id=id
+        except Usuario.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id, format=None):
+        '''
+        Tramitar peticion de Retorno de usuario identificado por id dado
+        '''
+
+        # 
+        usuario = self.get_object(id)
+        serializer = UserSerializer(usuario)
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        '''
+        Tramitar peticion de Actualizacion de usuario identificado por id dado
+        '''
+        pass 
+
+    def delete(self, request, id, format=None):
+        '''
+        Tramitar peticion de Eliminacion de usuario identificado por id dado
+        '''
+
