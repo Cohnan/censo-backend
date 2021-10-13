c2636bb11ee6e8505a5d0bcd65e56378ffefe88c
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-12 20:32:12 -0500

    Agregada funcionadad censo, sin login

--- censoIndigenasApp/urls.py
+++ censoIndigenasApp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import PersonaList
 from .views import PersonaDetail
+from .views import PersonaCrearView
 
 urlpatterns = [
     path('personas/', PersonaList.as_view()),
-    path('personas/<int:id>', PersonaDetail.as_view())
+    path('personas/<int:id>', PersonaDetail.as_view()),
+    path('personas/censar/', PersonaCrearView.as_view())
 ]
--- censoIndigenasApp/views/__init__.py
+++ censoIndigenasApp/views/__init__.py
@@ -1,5 +1,6 @@
 from .personaView import PersonaList
 from .personaView import PersonaDetail
+from .personaView import PersonaCrearView
 from .usuarioViews import UsuarioDetalleView
 from .usuarioViews import UsuarioListaView
 from .usuarioViews import UsuarioPersonalizadoView
