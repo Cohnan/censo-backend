40b5191cc3625a0597ffabddf6bb18da423e1505
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-13 00:43:59 -0500

    Added Ocupacion, Etnia and Resguardo models classes to their library inits

--- censoIndigenasApp/serializers/__init__.py
+++ censoIndigenasApp/serializers/__init__.py
@@ -1,2 +1,5 @@
 from .personaSerializer import PersonaSerializer
 from .usuarioSerializer import UserSerializer
+from .ocupacionSerializer import OcupacionSerializer
+from .etniaSerializer import EtniaSerializer
+from .resguardoSerializer import ResguardoSerializer
