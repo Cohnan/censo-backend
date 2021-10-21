812e2e2fb6dec67f2129c766e588b83f8366d56c
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-21 12:14:39 -0500

    Agregado Endpoint y View para busqueda de Persona por numero de doucmneto

--- censoIndigenasApp/urls.py
+++ censoIndigenasApp/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import PersonaListaView
 from .views import PersonaDetailView
 from .views import PersonaCrearView
+from .views import PersonaBuscarDocView
 
 urlpatterns = [
     path('personas/', PersonaListaView.as_view()),
     path('personas/<int:id>', PersonaDetailView.as_view()),
-    path('censar/', PersonaCrearView.as_view())
+    path('censar/', PersonaCrearView.as_view()),
+    path('buscar_doc/', PersonaBuscarDocView.as_view())
 ]
--- censoIndigenasApp/views/personaView.py
+++ censoIndigenasApp/views/personaView.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 #from django.shortcuts import get_object_or_404
-#from django.http import HttpResponse
 from django.http import Http404
-from rest_framework.views import APIView # Averiguar
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -96,3 +95,33 @@ class PersonaDetailView(APIView):
         return Response({
             "detail": "Persona eliminada exitosamente."
         }, status=status.HTTP_204_NO_CONTENT)
+
+class PersonaBuscarDocView(APIView):
+    '''
+    Procesa peticiones de búsqueda de personas por su documento único de identificación
+    '''
+
+    # Solo permitir el procesamiento de estas peticiones a quienes han iniciado sesión
+    permission_classes = (IsAuthenticated, )
+
+    def post(self, request):
+        # Extraer documento de la peticion
+        try:
+            tipo_doc_req = request.data["tipo_doc"]
+            numero_doc_req = request.data["doc_id"]
+        except:
+            return Response({"detail": "Formato de búsqueda inválido"}, status = status.HTTP_400_BAD_REQUEST)
+
+        # Traer a la persona
+        try:
+            persona = Persona.objects.get(tipo_doc = tipo_doc_req, doc_id = numero_doc_req)
+        except Persona.DoesNotExist:
+            return Response({"detail": "Persona no encontrada"}, status = status.HTTP_404_NOT_FOUND)
+
+        # Serializar a la persona corresopndiente para enviarlo como respuesta
+        serializer = PersonaSerializer(persona)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+
+        
+        
