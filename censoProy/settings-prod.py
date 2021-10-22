aa28e432eb6ed439228c01cdbef68afcaff90dce
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-22 02:45:43 -0500

    Correccion wsgi y settings para permitir deployment

--- censoProy/settings-prod.py
+++ censoProy/settings-prod.py
@@ -32,13 +32,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://censoindigena.herokuapp.com"]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080", 
-    "http://localhost:8081",
-    "http://localhost:8082",
-    "https://censofront.herokuapp.com"
-    ]
-# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+    # "http://localhost:8080", 
+    # "http://localhost:8081",
+    # "http://localhost:8082",
+    # "https://censofront.herokuapp.com"
+    # ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
